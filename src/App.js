2fe9c359a089fd05fd7dd23c6214cbd263eeedce
Kenan Tasdemir <bonox00@gmail.com>
2023-01-11 18:13:45 +0300

    lightning text commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 data-text1="&nbsp;♢ SLOWLY ♢&nbsp;">&nbsp;♢ SLOWLY ♢&nbsp;</h1>
+      <h2 data-text="&nbsp;♢ MOTION ♢&nbsp;">&nbsp;♢ MOTION ♢&nbsp;</h2>
+      <h3 data-text="&nbsp;♢ CODE STYLE ♢&nbsp;">&nbsp;♢ CODE STYLE ♢&nbsp;</h3>
     </div>
   );
 }
